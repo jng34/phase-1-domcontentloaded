1490cf5a9b11d513b5bdfb10aff5948f5cf4a6e0
JamesNg <njames15@gmail.com>
2022-05-02 09:56:48 -0400

    commit tree c930a4391067ab1503784cea7119228ff4405823

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+})
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    let p = document.querySelector('#text');
+    p.textContent = 'This is really cool!';
+})
+
